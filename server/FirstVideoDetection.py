e080c90e252627b8d61ca2f19263f8d1249df1de
BRUNDA SRIRAM <brusri@BRUNDAs-MacBook-Pro.local>
2020-02-06 11:37:48 +1100

    perfromance improvements

--- server/FirstVideoDetection.py
+++ server/FirstVideoDetection.py
@@ -38,7 +38,7 @@ def forFull(output_arrays, count_arrays, average_output_count):
 detector = VideoObjectDetection()
 detector.setModelTypeAsYOLOv3()
 detector.setModelPath(os.path.join(execution_path, "yolo.h5"))
-detector.loadModel(detection_speed="flash")
+detector.loadModel(detection_speed="faster")
 
 video_path = detector.detectObjectsFromVideo(input_file_path=os.path.join(execution_path, "input.mp4"),
                                              output_file_path=os.path.join(execution_path, "upload/output_detected_1"), frames_per_second=29,
@@ -48,7 +48,7 @@ video_path = detector.detectObjectsFromVideo(input_file_path=os.path.join(execut
                                              per_minute_function=forMinute,
                                              video_complete_function=forFull,
                                             
-                                             minimum_percentage_probability=10,
+                                             minimum_percentage_probability=20,
 
                                              log_progress=True)
 print(video_path)
